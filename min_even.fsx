752d62e476fc80ed48092dcfc9768d98f12986bc
ProgrammingBanana <daniel.jesus@upr.edu>
2020-11-07 12:33:41 -0400

    Added comments to min_even.fs

--- min_even.fsx
+++ min_even.fsx
@@ -1,5 +1,15 @@
-﻿
+﻿(*
+ * File: min_even.fs
+ * Author: Daniel De Jesus 802-16-1676
+ * Course COTI 4039 – Comparative Programming Languages
+ * Date: Noviembre 7, 2020
+ * Finds the value of the smallest even number if none are present
+ * it fails out of the program.
+ *)
 
+// indexOf: int list -> int
+// Finds the smallest even number by using List.filter and piping List.min. 
+// If no even numbers are found or the list is empty it fails out of the program
 let minEven lst =
     if List.isEmpty lst
         then failwith "FAIL: The list is empty"
@@ -7,12 +17,15 @@ let minEven lst =
             then failwith "FAIL: The list has no even numbers"
             else List.filter(fun elem -> elem % 2 = 0) lst |> List.min
 
-
+// Entry point of the program
 let lst = [30; 10; 50; 40; 20]
 let no_evens = [31; 11; 51; 41; 21]
 let empty = []
 
+// Testing minEven function
 printfn "The minimum even number is %d" (minEven lst)
-printfn "The minimum even number is %d" (minEven no_evens)
-printfn "The minimum even number is %d" (minEven empty)
+
+// Delete comments to test failwith in function
+// printfn "The minimum even number is %d" (minEven no_evens)
+// printfn "The minimum even number is %d" (minEven empty)
 
